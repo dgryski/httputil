14305f447f2fc9ba73e06b270d6ff96b49a32ad3
Damian Gryski <damian@gryski.com>
2014-03-31 10:21:46 +0200

    initial commit

---
+++ times.go
@@ -0,0 +1,20 @@
+package httputil
+
+import (
+	"net/http"
+	"time"
+)
+
+// SlowHandler wraps a http.HandlerFunc and calls cb if the request takes
+// longer than max time to process.  The function runs to completion -- slow
+// requests are not aborted.
+func SlowHandler(fn http.HandlerFunc, max time.Duration, cb func(r *http.Request, t time.Duration)) http.HandlerFunc {
+	return func(w http.ResponseWriter, req *http.Request) {
+		t0 := time.Now()
+		fn(w, req)
+		t := time.Since(t0)
+		if t > max {
+			cb(req, t)
+		}
+	}
+}
